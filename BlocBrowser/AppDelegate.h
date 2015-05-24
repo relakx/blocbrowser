6f483acf21429d90a8ebc23412abb58ded58a413
Joseph Blanco <joefblanco@gmail.com>
2015-05-24 18:11:46 -0400

    Initial Commit

---
+++ BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by Joseph Blanco on 5/24/15.
+//  Copyright (c) 2015 Blancode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlocBrowser/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BlocBrowser
+//
+//  Created by Joseph Blanco on 5/24/15.
+//  Copyright (c) 2015 Blancode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
