c0c07de2646607878858940b8f64790197eb28b3
Joseph Blanco <joefblanco@gmail.com>
2015-05-25 02:11:57 -0400

    Functional web browser

---
+++ BlocBrowser/BLCWebBrowserViewController.h
@@ -0,0 +1,13 @@
+//
+//  BLCWebBrowserViewController.h
+//  BlocBrowser
+//
+//  Created by Joseph Blanco on 5/24/15.
+//  Copyright (c) 2015 Blancode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCWebBrowserViewController : UIViewController
+
+@end
